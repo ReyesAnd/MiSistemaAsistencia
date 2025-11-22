eda3701facaede2374b20082a79fe4506735fb2d
Andry Biskel Reyes Peralta <100447633@est.uasd.edu.do>
2025-11-21 22:22:38 -0400

    Refactorizacion de la pestana de reportes ahora permitiendo generarlos en pantalla y ver diferentes reportes.

---
+++ MiSistemaAsistencia.Web/ViewModels/ReportViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace MiSistemaAsistencia.Web.ViewModels
+{
+    public class ReportViewModel
+    {
+        public DateTime StartDate { get; set; } = DateTime.Today;
+        public DateTime EndDate { get; set; } = DateTime.Today;
+        public string ReportType { get; set; } // "Asistencia", "Presentes", "Ausentes", "Tardanzas"
+        
+        public List<ReportItem> Results { get; set; } = new List<ReportItem>();
+    }
+
+    public class ReportItem
+    {
+        public string EmployeeName { get; set; }
+        public string EmployeeNumber { get; set; }
+        public string Department { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; } // "Presente", "Ausente", "Tarde"
+
+        public DateTime? CheckIn { get; set; }
+        public DateTime? CheckOut { get; set; }
+        public TimeSpan? ExpectedTime { get; set; }
+        public string Comments { get; set; }
+    }
+}
